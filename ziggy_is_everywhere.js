4391f6a21f7472dbdd2dbd7feef5de795fb58bd3
leo thibaudat <leo.thibaudat.16@eigsi.fr>
2019-03-13 19:36:02 -0400

    settinterval et check des images src pour palier au pages qui lazy load

--- ziggy_is_everywhere.js
+++ ziggy_is_everywhere.js
@@ -1,10 +1,35 @@
 
-const photos= ["https://ziggy.akeneo.com/assets/images/ziggy/sport3.JPG","https://ziggy.akeneo.com/assets/images/ziggy/adoption2.JPG","https://pbs.twimg.com/media/Ck8BsKzXEAAK1cY.jpg","https://scontent-atl3-1.cdninstagram.com/vp/d4c4c97929b1ab7f3988c130844e36cc/5D03F583/t51.2885-15/e35/41724500_725044884511840_2148682439255236590_n.jpg?_nc_ht=scontent-atl3-1.cdninstagram.com","https://ziggy.akeneo.com/assets/images/ziggy/sport4.JPG","https://ziggy.akeneo.com/assets/images/ziggy/education1.JPG","https://pbs.twimg.com/media/Dbo47EWU8AAlfi3.jpg","https://ziggy.akeneo.com/assets/images/ziggy/feeding1.JPG","https://pbs.twimg.com/media/Dbo47EWU8AAlfi3.jpg","https://pbs.twimg.com/media/DbbvTQiVMAAcYna.jpg", "https://ziggy.akeneo.com/assets/images/ziggy/theziggy1.JPG"];
-var imgTags = document.getElementsByTagName("img");
+(function(){
+	var nbImages = 0;
+	setInterval(function(){
+		var newnbimages = checkNbImagesOnPage();
+		if( newnbimages !== nbImages ){
+			replaceImages(0, returnsAllImagesOnPage());
+			nbImages = newnbimages
+		}
 
+	}, 2000)
+	const photos= ["https://ziggy.akeneo.com/assets/images/ziggy/adoption2.JPG", "https://ziggy.akeneo.com/assets/images/ziggy/adoption3.JPG","https://ziggy.akeneo.com/assets/images/ziggy/adoption1.JPG","https://ziggy.akeneo.com/assets/images/ziggy/adoption4.JPG","https://ziggy.akeneo.com/assets/images/ziggy/sport1.JPG","https://ziggy.akeneo.com/assets/images/ziggy/sport2.JPG","https://ziggy.akeneo.com/assets/images/ziggy/sport3.JPG","https://ziggy.akeneo.com/assets/images/ziggy/sport4.JPG","https://ziggy.akeneo.com/assets/images/ziggy/sport5.JPG","https://ziggy.akeneo.com/assets/images/ziggy/education1.JPG","https://ziggy.akeneo.com/assets/images/ziggy/feeding1.JPG","https://ziggy.akeneo.com/assets/images/ziggy/theziggy1.JPG"
+	];
+	
+	var imgTags = document.getElementsByTagName("img");
+	
+	replaceImages(nbImages, imgTags)
 
-for(var i = 0; i < imgTags.length; i++) {
-  var randomPic = photos[Math.floor(Math.random()*photos.length)];
-  imgTags[i].src = randomPic;
-} 
+	function replaceImages(startingPoint, imgTags){
+		for(var i = imgTags.length - 1 ; i >= startingPoint ;  i--) {
+			if(imgTags[i].src.indexOf("ziggy") == -1){
+			  	var randomPic = photos[Math.floor(Math.random()*photos.length)];
+			  	imgTags[i].src = randomPic;
+			}
+		}
+	}
 
+	function checkNbImagesOnPage(){
+		return returnsAllImagesOnPage().length;
+	}
+
+	function returnsAllImagesOnPage(){
+		return document.getElementsByTagName("img");
+	}
+})()
